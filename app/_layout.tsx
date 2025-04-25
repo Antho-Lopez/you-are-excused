eaf0bb7445613401f30760ce026efa0c97ca89fa
Lopez Anthony <70229078+Antho-Lopez@users.noreply.github.com>
2025-04-25 13:36:58 +0200

    -Style layout and index -Add excuses array -Add luckiestGuy font

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,30 @@
 import { Stack } from "expo-router";
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
+import { useFonts } from 'expo-font';
+import '@/utils/i18n';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  return <Stack />;
+
+  const [loaded] = useFonts({
+    LuckiestGuy: require('@/assets/fonts/LuckiestGuy-Regular.ttf'),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+    </Stack>
+  );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,89 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet, SafeAreaView, Image, TouchableOpacity } from "react-native";
+import colors from '@/constants/colors';
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
+        
+        <View style={styles.imageContainer}>
+          <Image
+              source={require('@/assets/images/logo.png')}
+              style={styles.image}
+          />
+        </View>
+
+        <View style={styles.contentContainer}>
+          <Text style={styles.title}>Trouve ton excuse</Text>
+          <Text style={styles.excuse}>L'excuse générée</Text>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>test</Text>
+          </TouchableOpacity>
+        </View>
+
+      </View>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: colors.darkBlue,
+  },
+  container: {
+    height: '80%',
+    margin: 20,
+    alignContent: 'center',
+    justifyContent: 'center',
+  },
+  imageContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginRight: 12,
+  },
+  image: {
+    width: 200,
+    height: 200,
+    borderRadius: 25,
+  },
+  contentContainer: {
+    marginTop: 60,
+    alignContent: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 30,
+    textAlign: 'center',
+    fontFamily: 'LuckiestGuy',
+    color: colors.yellow,
+    textShadowColor: 'black',
+    textShadowRadius: 3,
+    textShadowOffset: { width: -3, height: 4 },
+  },
+  excuse: {
+    marginTop: 20,
+    fontSize: 24,
+    textAlign: 'center',
+    fontFamily: 'LuckiestGuy',
+    color: colors.yellow,
+    textShadowColor: 'black',
+    textShadowRadius: 3,
+    textShadowOffset: { width: -3, height: 4 },
+  },
+  button: {
+    marginTop: 20,
+    backgroundColor: colors.red,
+    borderRadius: 25,
+  },
+  buttonText: {
+    padding: 10,
+    textAlign: 'center',
+    fontSize: 30,
+    fontFamily: 'LuckiestGuy',
+    color: colors.yellow,
+    textShadowColor: 'black',
+    textShadowRadius: 1,
+    textShadowOffset: { width: -3, height: 4 },
+  }
+});
